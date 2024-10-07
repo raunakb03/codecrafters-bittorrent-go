aa6ecb78770ff2dd03687b36c6dc9b7dfa7271ec
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2024-10-07 09:35:42 +0530

    decoded bencoded lists

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -5,50 +5,97 @@ import (
     "fmt"
     "os"
     "strconv"
-    "unicode"
 )
 
 var _ = json.Marshal
 
-func handleBencode(bencodedString string) (interface{}, error) {
-    var firstColenIndex int =-1 
+func decode(str string, index int) (interface{}, int, error) {
+    if index >= len(str) {
+        return nil, index, fmt.Errorf("Invalid string index %d", index)
+    }
 
-    for i:=0; i<len(bencodedString); i++ {
-        if bencodedString[i] == ':' {
-            firstColenIndex = i
+    switch {
+    case str[index] == 'l':
+        return decodeList(str, index)
+    case str[index] == 'i':
+        return decodeNumber(str, index)
+    case str[index] >= '0' && str[index] <= '9':
+        return decodeString(str, index)
+    default:
+        return nil, index, fmt.Errorf("Unexpected error value %q", str[index])
+    }
+}
+
+func decodeString(str string, index int) (interface{}, int, error){
+    var strLenStr string
+    for i:= index; i<len(str); i++ {
+        if str[i]==':' {
+            index = i+1
             break
         }
+        strLenStr += (string(str[i]))
     }
+    strLen, err := strconv.Atoi(strLenStr)
+    if err != nil {
+        fmt.Println("Error while converting string to number")
+        return nil, index, err
+    }
+    retStr := str[index: index+strLen]
+    index += strLen
+    return retStr, index, nil
+}
 
-    // decode a number
-    if firstColenIndex == -1 && bencodedString[0]=='i' {
-        numString := bencodedString[1:len(bencodedString)-1]
-        decodedNumber, err := strconv.Atoi(numString)
-        if err != nil {
-            return "", fmt.Errorf("Input string does not represent a number")
+func decodeNumber(str string, index int) (interface{}, int, error) {
+    var numStr string
+    for i := index + 1; i < len(str); i++ {
+        index = i
+        if str[i] == 'e' {
+            break
         }
-        return decodedNumber , nil
+        numStr += (string(str[i]))
+    }
+    decodedNumber, err := strconv.Atoi(numStr)
+    if err != nil {
+        fmt.Println("Error while converting string to number")
+        return nil, index, err
+    }
+    return decodedNumber, index+1, nil
+}
+
+func decodeList(str string, index int) (interface{}, int, error) {
+    index++
+
+    list :=  make([]interface{}, 0)
+
+    for {
+        if index>=len(str) {
+            return nil, index, fmt.Errorf("bad list")
+        }
+
+        if str[index] == 'e' {
+            break
+        }
+
+        var x interface{}
+
+        x, i, err := decode(str, index)
+        if err != nil {
+            return nil, index, err
+        }
+        index = i
+        list = append(list, x)
     }
 
-    stringLen := bencodedString[:firstColenIndex]
-
-    if !unicode.IsDigit(rune(stringLen[0])) {
-        return "", fmt.Errorf("bencoded string length can only be a number")
-    }
-
-    encodedString :=bencodedString[firstColenIndex+1:]
-
-    return encodedString, nil
+    return list, index, nil
 }
 
 func main() {
-
     command := os.Args[1]
 
     if command == "decode" {
         bencodedValue := os.Args[2]
 
-        decodedString, err := handleBencode(bencodedValue)
+        decodedString, _, err := decode(bencodedValue, 0)
         if err != nil {
             fmt.Println("Error while decoding the string ", err)
             return
